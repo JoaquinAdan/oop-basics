4cd932520f1175285e16c00275388501c939ed63
JoaquinAdan <joacorodriguez.98l@gmail.com>
2024-03-20 16:29:46 -0300

    Add object literals and constructor function

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('start')
+const joaquin = {
+  // objeto literal
+  name: 'Joaquin',
+  age: 25,
+  points: 1000,
+}
+function Student() {
+  this.name = 'Nombre'
+  this.age = 25
+  this.points = 1000
+}
+
+const joaco = new Student()
+console.log(joaco) // instancia de Student, no es un objeto literal
+console.log(joaquin)
+
+const objetito = {}
+
+console.log(joaco.hasOwnProperty('name'))
+console.log(objetito.hasOwnProperty('name'))
